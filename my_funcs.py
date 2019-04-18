d56b1217bdebc3a0156bec1eb0f843b02e62d16c
yashbhargava1992 <yash.bhargava1992@gmail.com>
2019-04-18 12:45:46 +0530

    Adding a function file

---
+++ my_funcs.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+def rebinner(x,y,rebin_factor,rate_flag):
+	"""
+	The function returns a rebinned array based on the rebin factor provided.
+	Currently the rebin factor should be a natural number. 
+	Support for log binning (via a negative number will be provided soon)
+	
+	
+	
+	
+	"""
+
