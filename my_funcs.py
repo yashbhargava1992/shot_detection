8ebccc676919e56a3e7147a4c851273949066741
yashbhargava1992 <yash.bhargava1992@gmail.com>
2019-04-18 15:50:21 +0530

    v1 DEtection of peaks in blocks of T secs done

--- my_funcs.py
+++ my_funcs.py
@@ -40,3 +40,46 @@ def data_extractor(fits_file_object):
 
 
 
+def peak_detector(time,rate,rerr,f=1,T=10):
+	"""
+	
+	Takes the light curve (with err on the rate) and returns the position of the peaks with which are certain fraction above the mean. 
+	Note: This doesn't fit the peak with anything. The computing is done using a simple comparison. The current version can detect peaks in discrete windows
+	For example the code will divide the LC into segments of length T and return the highest peaks in each segment. 
+	A more refined detection will be attempted later
+	
+	INPUT:
+	time						:Time array of the LC 
+	rate						:Rate array of the LC
+	rerr						:Error on the rate
+	f							:The factor by which the peak should be greater than the mean level, typically greater than 1
+	T							:The duration of the time for which the mean has to be computed. same unit as time
+	
+	
+	OUTPUT:
+	ind_time					: The index position of the peaks. 
+	"""
+	
+	total_duration = time[-1]-time[0]
+	number_of_segments = int(total_duration/T)
+	jump = len(time)/number_of_segments
+	peak_index_pos = np.array([],dtype=int)
+	for i in range(0,len(time),jump):
+		
+		if i+jump<=len(time):ind = np.arange (i,i+jump,1)
+		else : ind=np.arange (i,len(time),1)
+		seg_time = time[ind]
+		seg_rate = rate[ind]
+		seg_rerr = rerr[ind]
+		sig = np.median(seg_rerr)
+		peak_pos = int(np.argmax(seg_rate))
+		peak_val = seg_rate[peak_pos]					# This value is the maximum count in the segment
+		peak_time = seg_time[peak_pos]					# This value is the position of the peak in the segment. 
+		if peak_val>np.mean(seg_rate)*f:
+			peak_index_pos = np.append(peak_index_pos,int(ind[peak_pos]))
+		#~ else:
+			#~ peak_index_pos = np.append(peak_index_pos,np.nan)
+	peak_index_pos = peak_index_pos.astype(int)
+	return peak_index_pos
+	
+	 
--- shot_detector.py
+++ shot_detector.py
@@ -16,15 +16,15 @@ import my_funcs as mf
 
 data_path = "../lc_data/"
 
-unit1_data_band1	= fits.open(data_path+"laxpc_lc_5s_unit1_3.0_5.0keV.lc")
-unit1_data_band2	= fits.open(data_path+"laxpc_lc_5s_unit1_5.0_10.0keV.lc")
-unit1_data_band3 	= fits.open(data_path+"laxpc_lc_5s_unit1_10.0_20.0keV.lc")
-unit2_data_band1	= fits.open(data_path+"laxpc_lc_5s_unit2_3.0_5.0keV.lc")
-unit2_data_band2	= fits.open(data_path+"laxpc_lc_5s_unit2_5.0_10.0keV.lc")
-unit2_data_band3 	= fits.open(data_path+"laxpc_lc_5s_unit2_10.0_20.0keV.lc")
-unit3_data_band1	= fits.open(data_path+"laxpc_lc_5s_unit3_3.0_5.0keV.lc")
-unit3_data_band2	= fits.open(data_path+"laxpc_lc_5s_unit3_5.0_10.0keV.lc")
-unit3_data_band3 	= fits.open(data_path+"laxpc_lc_5s_unit3_10.0_20.0keV.lc")
+unit1_data_band1	= fits.open(data_path+"laxpc_lc_0p05_unit1_3.0_5.0keV.lc")
+unit1_data_band2	= fits.open(data_path+"laxpc_lc_0p05_unit1_5.0_10.0keV.lc")
+unit1_data_band3 	= fits.open(data_path+"laxpc_lc_0p05_unit1_10.0_20.0keV.lc")
+unit2_data_band1	= fits.open(data_path+"laxpc_lc_0p05_unit2_3.0_5.0keV.lc")
+unit2_data_band2	= fits.open(data_path+"laxpc_lc_0p05_unit2_5.0_10.0keV.lc")
+unit2_data_band3 	= fits.open(data_path+"laxpc_lc_0p05_unit2_10.0_20.0keV.lc")
+unit3_data_band1	= fits.open(data_path+"laxpc_lc_0p05_unit3_3.0_5.0keV.lc")
+unit3_data_band2	= fits.open(data_path+"laxpc_lc_0p05_unit3_5.0_10.0keV.lc")
+unit3_data_band3 	= fits.open(data_path+"laxpc_lc_0p05_unit3_10.0_20.0keV.lc")
 
 
 
@@ -37,4 +37,14 @@ unit3_time_band2, unit3_rate_band2, unit3_r_er_band2 = mf.data_extractor(unit3_d
 unit1_time_band3, unit1_rate_band3, unit1_r_er_band3 = mf.data_extractor(unit1_data_band3)
 unit2_time_band3, unit2_rate_band3, unit2_r_er_band3 = mf.data_extractor(unit2_data_band3)
 unit3_time_band3, unit3_rate_band3, unit3_r_er_band3 = mf.data_extractor(unit3_data_band3)
+
+
+jump = len(unit1_time_band1)/100
+for i in range(0,len(unit1_time_band1),jump):
+	
+	short_seg = np.arange(i,i+jump,1)
+	plt.errorbar(unit1_time_band1[short_seg], unit1_rate_band1[short_seg], yerr=unit1_r_er_band1[short_seg])
+	peak_pos = mf.peak_detector(unit1_time_band1[short_seg], unit1_rate_band1[short_seg], unit1_r_er_band1[short_seg],f=2,T=5)
+	plt.plot(unit1_time_band1[short_seg][peak_pos], unit1_rate_band1[short_seg][peak_pos],'o')
+	plt.show()
 
