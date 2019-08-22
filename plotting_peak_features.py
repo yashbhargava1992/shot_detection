dbdd860afbfd5e9ea1fa294837e36d658957c905
yashbhargava1992 <yash.bhargava1992@gmail.com>
2019-08-22 11:06:33 +0530

    Added the code to plot variation of the peak pars
    The code to variation of peak parameters with time was added.
    The parameters for different units are plotted in th same panel.

--- plotting_peak_features.py
+++ plotting_peak_features.py
@@ -12,7 +12,9 @@ rc('font',**{'size':14} )
 
 unit1_peak_features = np.loadtxt('unit1_peak_fit_values.txt')
 unit2_peak_features = np.loadtxt('unit2_peak_fit_values.txt')
+peak_time_from_file = np.loadtxt('peak_time_list_0p05_unit1.txt')
 
+print len(unit1_peak_features),len(peak_time_from_file)
 #~ plt.plot(unit1_peak_features[:,0],unit2_peak_features[:,0],'.')
 #~ plt.show()
 
@@ -69,4 +71,25 @@ for i in range (1,number_of_columns+1,1):
 		else: ax1.tick_params(labelleft=False)
 
 plt.savefig('Parameter_var_unit2_fitted_time.pdf')
+#~ plt.show()
+plt.clf()
+
+fig = plt.figure (figsize=(10,9))
+fig.subplots_adjust(hspace=0, wspace=0,top=0.95, bottom=0.1,left = 0.15)
+
+for i in range (1,number_of_columns+1):
+	if i==1 : 
+		ax1 = plt.subplot(number_of_columns,1,i)
+		ax1.plot(peak_time_from_file,np.abs(unit1_peak_features[:,i-1]),'.C0')
+		ax1.plot(peak_time_from_file,np.abs(unit2_peak_features[:,i-1]),'.C1')
+		ax1.set_ylabel(labels[i-1])
+	else : 
+		ax2 = plt.subplot(number_of_columns,1,i,sharex=ax1)
+		ax2.plot(peak_time_from_file,np.abs(unit1_peak_features[:,i-1]),'.C0')
+		ax2.plot(peak_time_from_file,np.abs(unit2_peak_features[:,i-1]),'.C1')
+		ax2.set_ylabel(labels[i-1])
+		ax2.set_yscale('log')
+	if i== number_of_columns:
+		ax2.set_xlabel('Time of the shot')
+
 plt.show()
