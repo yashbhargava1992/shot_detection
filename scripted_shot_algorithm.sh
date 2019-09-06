d0aafef0f2879232a048626daf7fe9aec064a636
yashbhargava1992 <yash.bhargava1992@gmail.com>
2019-09-06 11:10:26 +0530

    Added a bash script to run the programs in order
    A bash script is written to allow a user to run the codes in the order intended.
    Certain lines are commented as they are optional.

---
+++ scripted_shot_algorithm.sh
@@ -0,0 +1,9 @@
+# This code will run the codes for the shots in a sequence intended. 
+# Future versions could incorporate the passing of the arguments to the codes to generate output for a specific config
+
+python shot_detector.py
+python shot_fitting.py
+python shot_selector.py
+#python shot_ratio.py
+python shot_splitter.py
+#python plotting_routines.py
